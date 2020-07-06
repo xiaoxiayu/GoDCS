c151f522af3f12629349f25a0c13b08e0a0489a1
xiaoxia_yu <48938448@qq.com>
2020-07-06 10:35:22 +0800

    remove useless string

--- rpcserver_runner.go
+++ rpcserver_runner.go
@@ -1,4 +1,4 @@
-// Copyright 2015 xiaoxia_yu@foxitsoftware.com All rights reserved.
+// Copyright 2015 xiaoxia_yu@xxsoftware.com All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
